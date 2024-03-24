2eeccb3eb4586cba26c4d4401ac6462e821044b5
Kunal Gupta <kkunalguptaaa@gmail.com>
2024-03-24 08:09:46 +0000

    [Issue-1]: Fixed eslint config.

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,10 +1,28 @@
 module.exports = {
   root: true,
   env: { browser: true, es2020: true },
-  extends: ["eslint:recommended", "plugin:react-hooks/recommended"],
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:react-hooks/recommended",
+  ],
   ignorePatterns: ["dist", ".eslintrc.cjs"],
-  plugins: ["react-refresh"],
+  plugins: ["react-refresh", "react", "react-hooks"],
+  parserOptions: {
+    parser: "@babel/eslint-parser",
+    requireConfigFile: false,
+    sourceType: "module",
+    allowImportExportEverywhere: true,
+    ecmaFeatures: {
+      jsx: true,
+    },
+    babelOptions: {
+      presets: ["@babel/preset-react"],
+    },
+  },
   rules: {
+    "react/jsx-uses-react": "off",
+    "react/react-in-jsx-scope": "off",
     "react-refresh/only-export-components": [
       "warn",
       { allowConstantExport: true },
